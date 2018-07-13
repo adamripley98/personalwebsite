debc13b7c52f23fd71de48f52d48cfc469d7388f
Adam Ripley <aripley@wharton.upenn.edu>
2018-07-12 22:47:03 -0600

    procfile and linter

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const {createServer} = require('http');
+const { createServer } = require('http');
 const express = require('express');
 const compression = require('compression');
 const morgan = require('morgan');
@@ -14,19 +14,19 @@ if (!dev) {
   app.disable('x-powered-by');
   app.use(compression());
   app.use(morgan('common'));
-
-  app.use(express.static(path.resolve(__dirname, 'build')));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-  });
 } else {
   app.use(morgan('dev'));
 }
+
+app.use(express.static(path.resolve(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
 
 const server = createServer(app);
 
 server.listen(PORT, (err) => {
   if (err) throw err;
   console.log('Server started!');
-})
+});
